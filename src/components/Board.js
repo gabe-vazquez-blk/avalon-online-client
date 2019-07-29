53c17add0a6c4c48af3b2d52c81979934c83236a
Amy Cen <cenamy@gmail.com>
2019-07-29 19:56:10 -0400

    changed board based on player num

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Player from './Player'
 import QuestCoins from './QuestCoins'
 import VoteTrack from './VoteTrack'
 import {Grid} from 'semantic-ui-react'
+import {API_ROOT} from '../constants'
 
 class Board extends Component {
+
+  state = {
+    roles: [],
+    playerNum: parseInt(this.props.playerNum)
+  }
+
+  
+
+  playerCard = (playerNum) => {
+    const playerCards = []
+    for (let i=0; i<playerNum; i++){
+      playerCards.push((
+        <Grid.Column key={i}>
+          <Player />
+        </Grid.Column>
+      ))
+    }
+    return playerCards
+  }
 
   render() {
     return (
@@ -13,21 +33,7 @@ class Board extends Component {
         <br></br>
         <Grid verticalAlign='middle' columns={7} centered>
           <Grid.Row>
-            <Grid.Column>
-            <Player />
-            </Grid.Column>
-            <Grid.Column>
-              <Player />
-            </Grid.Column>
-            <Grid.Column>
-              <Player />
-            </Grid.Column>
-            <Grid.Column>
-              <Player />
-            </Grid.Column>
-            <Grid.Column>
-              <Player />
-            </Grid.Column>
+            {this.playerCard(this.state.playerNum)}
           </Grid.Row>
         </Grid>
         <br></br>
